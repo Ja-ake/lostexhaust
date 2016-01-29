74e6e47f5735c7cc7f415bd0b886901eaf623590
Jake Springer <me@jakespringer.com>
2016-01-28 16:38:47 -0800

    added ip checking

--- src/main/java/com/jakespringer/lostexhaust/auth/CatlinSessionService.java
+++ src/main/java/com/jakespringer/lostexhaust/auth/CatlinSessionService.java
@@ -18,7 +18,7 @@ public class CatlinSessionService implements SessionService {
     public UserSession getSession(String cookie, String ip) {
         CatlinCrypto.Key key = CatlinCrypto.getMessageFromString(cookie);
         if (key == null
-        		//|| !key.ip.equals(ip)
+        		|| !key.ip.equals(ip)
         		) {
             return null;
         } else {
@@ -29,7 +29,7 @@ public class CatlinSessionService implements SessionService {
             Optional<UserSession> possibleSession = sessions.stream().filter(x -> {
                 try {
                     return x.isStillValid() 
-                        //&& x.getIp().equals(ip)
+                        && x.getIp().equals(ip)
                         && x.getContext().getId().equals(key.id);
                 } catch (SessionExpiredException e) {
                     // should never happen
